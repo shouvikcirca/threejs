6c675ce85b0d950979009d2c2ce70a4c1801859b
rhettscronfinkle <goldenfibo1123@gmail.com>
2023-04-08 15:45:02 +0530

    Lesson 1.3: Introducing the World App

--- src/main.js
+++ src/main.js
@@ -1,62 +1,18 @@
-import {
-    BoxBufferGeometry,
-    Color,
-    Mesh,
-    MeshBasicMaterial,
-    PerspectiveCamera,
-    Scene,
-    WebGLRenderer,
-  } from "https://cdn.skypack.dev/three@0.132.2";
+import { World } from './World/World.js';
 
-  // Get a reference to the container element that will hold our scene
-const container = document.querySelector('#scene-container');
+// create the main function
+function main()
+{
+  const container = document.querySelector('#scene-container');
 
-// create a Scene
-const scene = new Scene();
-// Set the background color
-scene.background = new Color('skyblue');
+  // 1. Create an instance of the World app
+  const world = new World(container);
 
-// Create a camera
-const fov = 35; // AKA Field of View
-const aspect = container.clientWidth / container.clientHeight;
-const near = 0.1; // the near clipping plane
-const far = 100; // the far clipping plane
+  // 2. Render the scene
+  world.render();
+}
 
-const camera = new PerspectiveCamera(fov, aspect, near, far);
-
-camera.position.set(0, 0, 10);
-
-// create a geometry
-const geometry = new BoxBufferGeometry(2,2,2);
-
-// create a default (white) Basic material
-const material = new MeshBasicMaterial();
-
-const cube = new Mesh(geometry, material);
-
-// add the mesh to the scene
-scene.add(cube);    
-
-// create the renderer
-const renderer = new WebGLRenderer();
-
-
-// next, set the renderer to the same size as our container element
-renderer.setSize(container.clientWidth, container.clientHeight);
-
-// finally, set the pixel ratio so that our scene will look good on HiDPI displays
-renderer.setPixelRatio(window.devicePixelRatio);
-
-// add the automatically created <canvas> element to the page
-container.append(renderer.domElement);
-
-renderer.render(scene, camera);
-
-
-
-
-
-
-
+// call main to start the app
+main();
 
 
